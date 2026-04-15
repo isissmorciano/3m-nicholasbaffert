44baaa227c01b605795aabd05c83b6f37f003e0e
AlexanderBaffert <alexander.baffert@studenti.isissgobetti.it>
2026-04-15 13:49:28 +0200

    es67

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/baffert/3m-nicholasbaffert-10/.venv
+set -gx VIRTUAL_ENV /home/baffert/3m-nicholasbaffert-12/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
